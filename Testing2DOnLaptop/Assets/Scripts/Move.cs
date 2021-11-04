0d6e231873024bf36dd78fe5cf33818f146d71ba
Uzair Abdullah <uzairabdullah973@gmail.com>
2021-11-04 22:48:42 +0500

    first project

---
+++ Testing2DOnLaptop/Assets/Scripts/Move.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float v = Input.GetAxisRaw("Vertical");
+        float h = Input.GetAxisRaw("Horizontal");
+
+        transform.Translate(Vector2.right * Time.deltaTime * 9 * h);
+        transform.Translate(Vector2.up * Time.deltaTime * 9 * v);
+    }
+}
