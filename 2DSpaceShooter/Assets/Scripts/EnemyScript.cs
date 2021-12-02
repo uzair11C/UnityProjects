c79c73621c64da50594102ea4f2d1d60f5e4c7a8
Uzair Abdullah <uzairabdullah973@gmail.com>
2021-12-02 21:53:52 +0500

    bruh

--- 2DSpaceShooter/Assets/Scripts/EnemyScript.cs
+++ 2DSpaceShooter/Assets/Scripts/EnemyScript.cs
@@ -4,15 +4,64 @@ using UnityEngine;
 
 public class EnemyScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed = 7f;
+    public float rotate_speed = 50f;
+
+    private Rigidbody2D rb;
+
+    private Animator anim;
+
+    public bool canRotate;
+    public bool canShoot;
+    //private bool canMove = true;
+
+    public float bound_y = -11f;
+    //public Transform attack_point;
+    //public GameObject enemy_bullet;
+
+
+    void Awake()
+    {
+        anim= this.GetComponent<Animator>();
+    }
+
     void Start()
     {
-        
+        rb = this.GetComponent<Rigidbody2D>();
+        if(canRotate)
+        {
+            if(Random.Range(0 , 2) > 0)
+            {
+                rotate_speed = Random.Range(rotate_speed , rotate_speed + 20f);
+                rotate_speed *= -1f;
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+        RotateEnemy();
+    }
+
+    void Move()
+    {
+
+        var vector3 = new Vector3(0f, -speed * Time.deltaTime,0f);
+        rb.velocity = vector3;
+
+        if(rb.transform.position.y > bound_y)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    void RotateEnemy()
+    {
+        if(canRotate)
+        {
+            transform.Rotate(new Vector3(0f , 0f , rotate_speed * Time.deltaTime), Space.World);
+        }
     }
 }
