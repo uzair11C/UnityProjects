679a4d3c89a803058c0135f0fb1a7f8d74db64f6
Uzair Abdullah <uzairabdullah973@gmail.com>
2022-03-11 16:00:45 +0500

    pacing up done

--- QuizGame/Assets/Scripts/GameOver.cs
+++ QuizGame/Assets/Scripts/GameOver.cs
@@ -1,11 +1,19 @@
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class GameOver : MonoBehaviour
 {
+    [SerializeField]
+    private Text h_score;
+
+    void Start()
+    {
+        h_score.text = "High Score: " + Manager.high_score;
+    }
+
     public void restart()
     {
         SceneManager.LoadScene("MainGameplay");
     }
-    
 }
--- QuizGame/Assets/Scripts/Manager.cs
+++ QuizGame/Assets/Scripts/Manager.cs
@@ -7,8 +7,6 @@ public class Manager : MonoBehaviour
 {
     public Callers[] callers;
 
-    public Text caller;
-
     public Text answer;
 
     [SerializeField]
@@ -17,9 +15,24 @@ public class Manager : MonoBehaviour
     [SerializeField]
     private GameObject decline_button;
 
+    [SerializeField]
+    private Text c_score;
+
+    [SerializeField]
+    private Text timer;
+
+    public Text caller;
+
+    private static int current_score;
+    public static int high_score;
+
+    [SerializeField]
+    private float ans_time;
+
     void Start()
     {
-        //getRandomCaller();
+        current_score = 0;
+        high_score = PlayerPrefs.GetInt("HighScore", 0);
         StartCoroutine(transition());
     }
 
@@ -27,6 +40,8 @@ public class Manager : MonoBehaviour
 
     void getRandomCaller()
     {
+        
+        timer.text = "" + ans_time +"s";
         answer_button.SetActive(true);
         decline_button.SetActive(true);
 
@@ -35,15 +50,33 @@ public class Manager : MonoBehaviour
         caller.text = currentCaller.caller_type;
     }
 
+    void Update()
+    {
+        c_score.text = "" + current_score;
+        
+        if (caller.text != "")
+        {
+            timer.text = "" + Mathf.Round(ans_time) + "s";
+            ans_time -= 1 * Time.deltaTime;
+        }
+
+        if (ans_time < 0)
+        {
+            SceneManager.LoadScene("GameOver");
+        }
+    }
+
     IEnumerator transition()
     {
         caller.text = "";
         answer_button.SetActive(false);
-        decline_button.SetActive(false);
+        decline_button.SetActive(false);        
+        timer.text = "";
 
-        yield return new WaitForSeconds(2);
+        yield return new WaitForSeconds(Random.Range(2,5));
 
         answer.text = "";
+        timer.text = "";
 
         getRandomCaller();
     }
@@ -53,6 +86,16 @@ public class Manager : MonoBehaviour
         if (currentCaller.answerable)
         {
             answer.text = "Correct!";
+
+            current_score++;
+
+            if (current_score > PlayerPrefs.GetInt("HighScore", 0))
+            {
+                PlayerPrefs.SetInt("HighScore", current_score);
+                high_score = current_score;
+                ans_time = ans_time - 2.0f;
+            }
+
             StartCoroutine(transition());
         }
         else
@@ -67,6 +110,16 @@ public class Manager : MonoBehaviour
         if (!currentCaller.answerable)
         {
             answer.text = "Correct!";
+
+            current_score++;
+
+            if (current_score > PlayerPrefs.GetInt("HighScore", 0))
+            {
+                PlayerPrefs.SetInt("HighScore", current_score);
+                high_score = current_score;
+                ans_time = ans_time - 2.0f;
+            }
+
             StartCoroutine(transition());
         }
         else
